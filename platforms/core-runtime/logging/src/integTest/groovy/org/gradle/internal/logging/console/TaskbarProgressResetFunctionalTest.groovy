4c42efe9517da8fa267c1fd0317f5444427330ca
Bo Zhang <bo@gradle.com>
2026-03-31 16:25:11 +0800

    Ignore TaskbarProgressResetFunctionalTest for now

--- platforms/core-runtime/logging/src/integTest/groovy/org/gradle/internal/logging/console/TaskbarProgressResetFunctionalTest.groovy
+++ platforms/core-runtime/logging/src/integTest/groovy/org/gradle/internal/logging/console/TaskbarProgressResetFunctionalTest.groovy
@@ -23,6 +23,7 @@ import org.gradle.test.preconditions.IntegTestPreconditions
 import org.gradle.test.preconditions.UnitTestPreconditions
 import org.gradle.test.precondition.Requires
 import spock.lang.Issue
+import spock.lang.Ignore
 
 /**
  * Verifies that the OSC 9;4;0 taskbar progress reset sequence is emitted when a build ends,
@@ -48,10 +49,12 @@ class TaskbarProgressResetFunctionalTest extends AbstractIntegrationSpec {
             .withConsole(ConsoleOutput.Rich)
     }
 
+    @Ignore('https://github.com/gradle/gradle-private/issues/5153')
     @Requires(value = [UnitTestPreconditions.Unix, IntegTestPreconditions.NotEmbeddedExecutor],
         reason = "sends SIGINT to a forked process works only on Unix and with a separate process")
     def "sends OSC 9;4;0 reset sequence when build receives SIGINT"() {
         given:
+        def timeoutS = 60
         // The task creates a marker file once it's running, then sleeps.
         // We wait for the marker before sending SIGINT to avoid racing
         // against JVM signal-handler setup on slow CI machines.
@@ -61,7 +64,7 @@ class TaskbarProgressResetFunctionalTest extends AbstractIntegrationSpec {
                 def marker = file("${readyFile.name}")
                 doFirst {
                     marker.createNewFile()
-                    Thread.sleep(600_000)
+                    Thread.sleep(${timeoutS}_000)
                 }
             }
         """
@@ -69,7 +72,7 @@ class TaskbarProgressResetFunctionalTest extends AbstractIntegrationSpec {
         when:
         def gradle = executer.withTasks("block").start()
 
-        ConcurrentTestUtil.poll {
+        ConcurrentTestUtil.poll(timeoutS) {
             assert readyFile.exists()
         }
 
