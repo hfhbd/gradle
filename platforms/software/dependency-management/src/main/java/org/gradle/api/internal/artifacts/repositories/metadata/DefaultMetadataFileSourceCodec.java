376214a75542ed2903c8966e7c0e73a672dd3073
Justin Van Dort <jvandort@gradle.com>
2026-04-16 18:18:20 -0400

    Revert "Move GroupedAndNamedUniqueFileStore to dependency-management"
    This reverts commit 330826c8814eb7e02ff5944088ce2d5fbc36c4c8.

--- platforms/software/dependency-management/src/main/java/org/gradle/api/internal/artifacts/repositories/metadata/DefaultMetadataFileSourceCodec.java
+++ platforms/software/dependency-management/src/main/java/org/gradle/api/internal/artifacts/repositories/metadata/DefaultMetadataFileSourceCodec.java
@@ -39,9 +39,9 @@ import java.io.IOException;
  */
 public class DefaultMetadataFileSourceCodec implements PersistentModuleSource.Codec<MetadataFileSource> {
     private final ImmutableModuleIdentifierFactory moduleIdentifierFactory;
-    private final ArtifactIdentifierFileStore<ModuleComponentArtifactIdentifier> fileStore;
+    private final ArtifactIdentifierFileStore fileStore;
 
-    public DefaultMetadataFileSourceCodec(ImmutableModuleIdentifierFactory moduleIdentifierFactory, ArtifactIdentifierFileStore<ModuleComponentArtifactIdentifier> fileStore) {
+    public DefaultMetadataFileSourceCodec(ImmutableModuleIdentifierFactory moduleIdentifierFactory, ArtifactIdentifierFileStore fileStore) {
         this.moduleIdentifierFactory = moduleIdentifierFactory;
         this.fileStore = fileStore;
     }
