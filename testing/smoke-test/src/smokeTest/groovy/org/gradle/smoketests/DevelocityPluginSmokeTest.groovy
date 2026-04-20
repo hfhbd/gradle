3cbceaa5659f770268caa2f31dc0adcbe6f36a22
Pavlo Shevchenko <pshevchenko@gradle.com>
2026-04-20 11:43:00 +0200

    chore: update Develocity Gradle plugin to 4.4.1

--- testing/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/DevelocityPluginSmokeTest.groovy
+++ testing/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/DevelocityPluginSmokeTest.groovy
@@ -163,7 +163,8 @@ class DevelocityPluginSmokeTest extends AbstractSmokeTest {
         "4.3",
         "4.3.1",
         "4.3.2",
-        "4.4.0"
+        "4.4.0",
+        "4.4.1"
     ]
 
     // Current injection scripts support Develocity plugin 3.6.4 and above
